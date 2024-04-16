48653d1583ca1e7b2c70ea3aaee897be46fa8089
Mouad <130792324+mouadiscoding@users.noreply.github.com>
2024-04-16 19:42:14 +0100

    commit

--- TP1/graphe.cpp
+++ TP1/graphe.cpp
@@ -132,4 +132,47 @@ namespace explorationGraphe {
     detruireListe(li);
     free(coutAcces);
     }
+    
+    // Exploration A* d'un graphe.
+    void explorationAStar(GrapheMat* graphe, int numSommet, char* but) {
+        if (numSommet < 0 || numSommet >= graphe->n) {
+            cout << "Numéro de sommet invalide\n";
+            return;
+        }
+        initMarque(graphe);
+        int* coutAcces = (int*)malloc(graphe->n * sizeof(int));
+        for (int i = 0; i < graphe->n; i++) {
+            coutAcces[i] = INFINI;
+        }
+        Liste* li = creerListe(1, NULL, NULL);
+        insererEnTeteDeListe(li, graphe->nomS[numSommet]);
+        coutAcces[numSommet] = 0;
+
+        while (!listeVide(li)) {
+            char* sommetCourant = (char*)extraireEnTeteDeListe(li);
+            cout << sommetCourant << " coût : " << coutAcces[rang(graphe, sommetCourant)];
+
+            if (strcmp(sommetCourant, but) == 0) {
+                cout << "(but trouvée)";
+                return;
+            }
+
+            int indiceSommetCourant = rang(graphe, sommetCourant);
+            graphe->marque[indiceSommetCourant] = vrai;
+
+            for (int i = 0; i < graphe->n; i++) {
+                if (graphe->element[indiceSommetCourant * graphe->nMax + i] == vrai) {
+                    int nouveauCout = coutAcces[indiceSommetCourant] + graphe->valeur[indiceSommetCourant * graphe->nMax + i];
+                    if (nouveauCout < coutAcces[i]) {
+                        coutAcces[i] = nouveauCout;
+                        insererEnOrdre(li, graphe->nomS[i]);
+                    }
+                }
+            }
+        }
+
+        detruireListe(li);
+        free(coutAcces);
+    }
+    
 }
